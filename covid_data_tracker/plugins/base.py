974d412376c42099970e1f0c584fc195bb86c54b
Siddharth Gupta <sid@granular.ai>
2020-06-06 18:14:29 -0400

    cleaning up base

--- covid_data_tracker/plugins/base.py
+++ covid_data_tracker/plugins/base.py
@@ -49,10 +49,10 @@ class BasePlugin:
         self.country_row = {}
         self.absolute_gender_calculations('absolute_cases')
         self.percent_gender_calculations('percent_cases', 'absolute_cases')
-        # self._absolute_case_calculations()
-        # self._percent_case_calculations()
-        #
-        # self._absolute_death_calculations()
+
+        self.absolute_gender_calculations('absolute_deaths')
+        self.percent_gender_calculations('percent_deaths', 'absolute_deaths')
+
         # self._percent_death_calculations()
 
         # self._absolute_tested_calculations()
@@ -97,8 +97,8 @@ class BasePlugin:
         total_percent_column, female_percent_column, male_percent_column = self._get_breakdown_columns(percent_column)
 
         if self.country_row[female_absolute_column] and self.country_row[male_absolute_column]:
-            self.country_row[female_percent_column] = self.country_row[female_absolute_column] / self.country_row[total_absolute_column]
-            self.country_row[male_percent_column] = self.country_row[male_absolute_column] / self.country_row[total_absolute_column]
+            self.country_row[female_percent_column] = self.country_row[female_absolute_column] / self.country_row[total_absolute_column] * 100
+            self.country_row[male_percent_column] = self.country_row[male_absolute_column] / self.country_row[total_absolute_column] * 100
         elif self.sex_table[percent_column]['female'] and self.sex_table[percent_column]['male']:
             self.country_row[female_percent_column] = self.sex_table[percent_column]['female']
             self.country_row[male_percent_column] = self.sex_table[percent_column]['male']
@@ -106,41 +106,6 @@ class BasePlugin:
             self.country_row[female_percent_column] = None
             self.country_row[male_percent_column] = None
 
-    def _absolute_case_calculations(self):
-        # Total no. of cases:
-        # if disaggregated by sex, this should be sum of male + female cases.
-        # If not disaggregated by sex, total number of cases
-
-        # if absolute breakdown available by gender...
-        # then set rows to these values
-        # and set total to the sum of these values
-
-        # else if only the total is available
-        # set total to this value, and set breakdown values as N/A
-        # else set everything as N/A
-
-        if self.sex_table['absolute_cases']['male'] and self.sex_table['absolute_cases']['female']:
-            self.country_row['no. of cases (men)'] = self.sex_table['absolute_cases']['male']
-            self.country_row['no. of cases (women)'] = self.sex_table['absolute_cases']['female']
-            self.country_row['Total no. of cases'] = self.country_row['no. of cases (men)'] + self.country_row['no. of cases (women)']
-        elif self.sex_table['absolute_cases']['total']:
-            self.country_row['no. of cases (men)'] = None
-            self.country_row['no. of cases (women)'] = None
-            self.country_row['Total no. of cases'] = self.sex_table['absolute_cases']['total']
-        else:
-            self.country_row['Total no. of cases'] = None
-
-    def _percent_case_calculations(self):
-        # % cases (men) and (female)
-        if self.country_row['no. of cases (men)'] and self.country_row['no. of cases (women)']:
-            self.country_row["% cases (men)"] = self.country_row['no. of cases (men)'] / self.country_row['Total no. of cases']
-            self.country_row["% cases (female)"] = self.country_row['no. of cases (women)'] / self.country_row['Total no. of cases']
-        elif self.sex_table['percent_cases']['male'] and self.sex_table['percent_cases']['female']:
-            self.country_row["% cases (men)"] = self.sex_table['percent_cases']['male']
-            self.country_row["% cases (female)"] = self.sex_table['percent_cases']['female']
-        else:
-            self.country_row["% cases (men)"] = None
-            self.country_row["% cases (female)"] = None
 
     def _absolute_death_calculations(self):
         # Total no. of deaths:
