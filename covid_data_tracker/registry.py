7a021bb1ab42269c03948721ff23324f13086147
Siddharth Gupta <sid@granular.ai>
2020-06-23 17:15:44 -0400

    added try, except to prevent broken plugins from making tool unusable

--- covid_data_tracker/registry.py
+++ covid_data_tracker/registry.py
@@ -3,13 +3,17 @@ import glob
 import importlib
 
 from covid_data_tracker.plugins.base import BasePlugin
-
+import logging
 
 plugin_modules = glob.glob('**/countries/[!_]*.py', recursive=True)
 base_module_dir = 'covid_data_tracker.plugins.countries.'
 
 for mod_path in plugin_modules:
-    mod_file = os.path.basename(mod_path)
-    importlib.import_module(base_module_dir + mod_file.strip(".py"))
+    try:
+        mod_file = os.path.basename(mod_path)
+        importlib.import_module(base_module_dir + mod_file.strip(".py"))
+    except Exception as e:
+        logging.warning(f"unable to import {mod_path}")
+        logging.warning(e)
 
 PluginRegistry = BasePlugin.PluginRegistry
